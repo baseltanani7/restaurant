baa6211ce07b6673d20c43e2b34b0a45e32cb0ba
Basel Tanani <baseltanani9@gmail.com>
2023-06-03 15:37:55 +0300

    add page about us

---
+++ js/about.js
@@ -0,0 +1,9 @@
+$(window).on('load' ,function() {
+    setTimeout(function(){
+        $('.parent-load').addClass('remove');
+    }, 2000);
+});
+
+// $(window).on('load' ,function() {
+//     $('.parent-load').fadeOut(9000);
+// });
